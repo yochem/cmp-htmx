c5366afac02e04381784881821624608a52e2a66
Yochem van Rosmalen <git@yochem.nl>
2023-08-10 22:35:54 +0200

    Add more data to the completion items
    Using LSP specification as reference:
    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#completionItem

--- scripts/update.py
+++ scripts/update.py
@@ -1,12 +1,11 @@
-from collections import namedtuple
 from contextlib import contextmanager
+import os
 from pathlib import Path
 import subprocess
 import shutil
-from pprint import pprint
+import typing
 
-
-Pathable = Path | str
+Pathlike = typing.Union[os.PathLike, str]
 
 
 @contextmanager
@@ -14,7 +13,6 @@ def htmx_repo_clone(target_dir: Path = None) -> Path:
     if not target_dir:
         target_dir = Path("./htmx")
 
-    # git clone
     subprocess.run(
         [
             "git",
@@ -33,25 +31,32 @@ def htmx_repo_clone(target_dir: Path = None) -> Path:
             shutil.rmtree(target_dir)
 
 
-def extract_docs(doc_file: Pathable) -> str:
+def extract_docs(doc_file: Pathlike) -> str:
     content = Path(doc_file).read_text()
     paragraphs = content.split("\n\n")
-    return paragraphs[1].replace('\n', '\\n')
+    return paragraphs[1].replace("\n", "\\n")
 
 
-def extract_attributes(repo: Pathable) -> list[dict[str, str]]:
+def extract_attributes(repo: Pathlike) -> list[dict[str, str]]:
     attributes = []
 
     attribute_dir = Path(repo).joinpath("www", "content", "attributes")
+
+    # completion kind corresponds to 'Value', like vscode-html-server does
+    # for other attributes
+    # (See LSP documentation: #completionItemKind)
+    completion_kind_value = 12
 
     for doc_file in attribute_dir.glob("hx-*"):
         name = doc_file.stem
         attributes.append(
             {
                 "label": name,
-                "insertText": f'{name}=""',
+                "insertText": f'{name}="$1"$0',
                 "filterText": name,
-                "documentation": extract_docs(doc_file)
+                "documentation": extract_docs(doc_file),
+                "insertTextFormat": 2,
+                "kind": completion_kind_value
             }
         )
 
@@ -64,13 +69,17 @@ def to_lua(
     overwrite: bool = False,
 ):
     def item_to_lua(item):
-        fields = ", ".join(fr"{key} = '{val}'" for key, val in item.items())
+        # repr() is a hack to add quotes around string values
+        fields = ", ".join(rf"{key} = {repr(val)}" for key, val in item.items())
         return "{ " + fields + " }"
 
     start = "return {"
     end = "}"
     items = ",\n\t".join(item_to_lua(item) for item in cmp_data)
 
+    target_file = Path(target_file)
+    # if target_file.exists() and not overwrite:
+    #     raise
     Path(target_file).write_text(f"{start}\n{items}\n{end}\n")
 
 
@@ -78,4 +87,4 @@ if __name__ == "__main__":
     dev = True
     with htmx_repo_clone() as htmx_dir:
         attr_items = extract_attributes(htmx_dir)
-        print(to_lua(attr_items))
+        to_lua(attr_items, target_file='../lua/attribute-list.lua')
